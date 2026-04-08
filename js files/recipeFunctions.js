d70a884650ecd5de4f43bd4a71fbb1d2dedecaae
rhunt2911 <huntrachel.work@gmail.com>
2026-04-08 15:00:11 +0100

    Created skeleton for recipe objects

---
+++ js files/recipeFunctions.js
@@ -0,0 +1,39 @@
+//modal information base for each recipe page based on id
+/**
+ * id: {
+ *      title: "",
+ *      time: "",
+ *      tags: [
+ *          "",
+ *      ],
+ *      favourite: ,
+ *      link: "",
+ *      ingredients: [
+ *          "",
+ *      ],
+ *      instructions: [
+ *          "",
+ *      ]
+ * }
+ */
+
+const recipesObj = {
+    LentilDahl: {
+        title: "Lentil Dahl",
+        time: "1 hour",
+        tags: [
+            "vegan",
+            "one pot"
+        ],
+        favourite: false, 
+        link: "https://www.noracooks.com/red-lentil-dahl/",
+        ingredients: [
+            "lentils",
+            "coconut milk"
+        ],
+        instructions: [
+            "test instruction 1",
+            "test instruction 2"
+        ],
+    },
+}
