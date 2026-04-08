b353365a2b65b74e9b04e70f3cecdf9ee4d9e960
rhunt2911 <huntrachel.work@gmail.com>
2026-04-08 13:38:58 +0100

    Adjusted nav bar to be modular, and made search bar width adaptive

--- js files/customComponents.js
+++ js files/customComponents.js
@@ -8,11 +8,30 @@ class HamburgerIcon extends HTMLElement {
                 <div class="topHamBar"></div>
                 <div class="midHamBar"></div>
                 <div class="bottomHamBar"></div>
-            </div>
-        `;
+            </div>`;
     }
 }
 customElements.define("hamburger-icon", HamburgerIcon);
+
+/*navigation bar*/
+class NavigationBar extends HTMLElement {
+    connectedCallback(){
+        const navText = this.getAttribute("navText");
+
+        this.innerHTML = `
+            <div class="navBar row centerVertical">
+               <hamburger-icon></hamburger-icon>
+               <h1>${navText}</h1>
+               <div class="searchbarContainer centerVertical">
+                    <form action="/recipes-list.html">
+                        <input type="text" placeholder="search here...">
+                        <button type="submit">go!</button>
+                    </form>
+                </div>
+            </div>`;
+    }
+}
+customElements.define("nav-bar", NavigationBar);
 
 /*links on home to different pages in site*/
 class HomeLink extends HTMLElement{
