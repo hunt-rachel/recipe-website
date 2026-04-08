d39c64e584a954f4eaf609cd4fb8a6f1ef31bea6
rhunt2911 <huntrachel.work@gmail.com>
2026-04-08 13:21:58 +0100

    Created home page skeleton

--- js files/customComponents.js
+++ js files/customComponents.js
@@ -1,4 +1,6 @@
 /*file for creating custom, modular components*/
+
+/*icon for navigation bar*/
 class HamburgerIcon extends HTMLElement {
     connectedCallback(){
         this.innerHTML = `
@@ -11,3 +13,17 @@ class HamburgerIcon extends HTMLElement {
     }
 }
 customElements.define("hamburger-icon", HamburgerIcon);
+
+/*links on home to different pages in site*/
+class HomeLink extends HTMLElement{
+    connectedCallback(){
+        const url = this.getAttribute("url");
+        const linkText = this.getAttribute("linkText")
+        
+        this.innerHTML = `
+            <div class="homeLinks">
+                <p><a href="${url}">${linkText}</a></p>
+            </div>`;
+    }
+}
+customElements.define("home-link", HomeLink);
